3f78148dea8fca729cba9ba5f588a1f0efacaf1d
melodic456 <137816046+melodic456@users.noreply.github.com>
2024-12-09 15:39:23 +0600

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,13 @@
+from functools import wraps
+from flask import session, redirect, url_for
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'logged_in' not in session:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+def check_credentials(username, password):
+    return username == "melodic" and password == "mypassword"
